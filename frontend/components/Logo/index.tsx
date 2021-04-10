994738eea890021f3e838eef94aad33c7238df7f
Benjamin Paige <benjamin.paige@gmail.com>
2021-04-10 16:22:43 -0600

    a better snail

--- frontend/components/Logo/index.tsx
+++ frontend/components/Logo/index.tsx
@@ -1,13 +1,12 @@
 import { Heading, WrapItem, useColorModeValue as mode } from "@chakra-ui/react"
-import { FaFire } from "react-icons/fa"
-import { GiSnail } from "react-icons/gi"
+import { GiSpikedSnail } from "react-icons/gi"
 import { IconContext } from "react-icons"
 
 export const Logo = () => {
   return (
     <WrapItem>
       <IconContext.Provider value={{ color: mode("blue.600", "blue.200") }}>
-        <GiSnail size="24px" />
+        <GiSpikedSnail size="24px" />
       </IconContext.Provider>
       <Heading ml="2" mt="2px" size="md">
         Snail Sender
