af9affc66e27e01c1e1e68a70151802e829244db
Benjamin Paige <benjamin.paige@gmail.com>
2021-10-24 15:35:30 -0600

    Add remove from cart functionality

--- frontend/components/Cart/index.tsx
+++ frontend/components/Cart/index.tsx
@@ -1,10 +1,36 @@
 import { useUser } from '@/hooks/useUser'
 import * as Chakra from '@chakra-ui/react'
+import { REMOVE_FROM_CART_MUTATION } from '@/graphql'
+import { useMutation, Cache } from '@apollo/client'
 
+interface RemoveCartItemProps {
+    cartItemId: string
+}
+
+const RemoveCartItem = ({cartItemId}: RemoveCartItemProps) => {
+    const [removeFromCart, { loading }] = useMutation(REMOVE_FROM_CART_MUTATION, {
+        variables: {id: cartItemId},
+        
+        // optimistic update
+        update(cache, payload) {
+            cache.evict(cache.identify(payload.data.deleteCartItem) as Cache.EvictOptions)
+        }
+    })
+
+    const handleRemoveFromCart = () => {
+        removeFromCart()
+    }
+
+    return (
+        <Chakra.Button size="sm" variant="outline" disabled={loading} onClick={handleRemoveFromCart}>
+            X
+        </Chakra.Button>
+    )
+}
 
 const CartItem = ({cartItem}) => {
     return (
-        <Chakra.Flex py="2" my="2" px={{ base: "4", md: "8" }} backgroundColor={Chakra.useColorModeValue("gray.100", "gray.600")} borderRadius="md"> 
+        <Chakra.Flex py="2" my="2" px="2" backgroundColor={Chakra.useColorModeValue("gray.100", "gray.600")} borderRadius="md"> 
             <Chakra.Box fontSize="small">
                 <Chakra.Text>{cartItem.letter.recipientName}</Chakra.Text>
                 <Chakra.Text>{cartItem.letter.addressLine1}</Chakra.Text>
@@ -13,9 +39,7 @@ const CartItem = ({cartItem}) => {
             </Chakra.Box>
             <Chakra.Spacer/>
             <Chakra.Box>
-                <Chakra.Button size="xs" variant="outline">
-                    X
-                </Chakra.Button>
+                <RemoveCartItem cartItemId={cartItem.id}/>
             </Chakra.Box>
         </Chakra.Flex>
     )
@@ -31,12 +55,18 @@ const Cart = () => {
             {me.user.cart.map(cartItem => {
                 return <CartItem key={cartItem.id} cartItem={cartItem}/>
             })}
-            <Chakra.Divider py="2"/>
-            <Chakra.Flex py="2">
-            <Chakra.Heading as="h3" size="md">Total</Chakra.Heading>
-            <Chakra.Spacer/>
-            <Chakra.Heading as="h3" size="md">${2*me.user.cart.length}</Chakra.Heading>
-            </Chakra.Flex>
+            {me.user.cart.length > 0 ? (
+                <Chakra.Box>
+                    <Chakra.Divider py="2"/>
+                    <Chakra.Flex py="2">
+                        <Chakra.Heading as="h3" size="md">Total</Chakra.Heading>
+                        <Chakra.Spacer/>
+                    <Chakra.Heading as="h3" size="md">${2*me.user.cart.length}</Chakra.Heading>
+                    </Chakra.Flex>
+                </Chakra.Box>)
+                :
+                <Chakra.Heading as="h3" size="md">No Items in Cart</Chakra.Heading>
+            }
         </Chakra.Box>
     )
 }
