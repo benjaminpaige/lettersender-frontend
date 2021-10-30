c9d95652f8db55f28336da796558dd5157b92ef1
Paul Kim <pykim0591@gmail.com>
2021-10-30 07:25:31 -0600

    Apollo client initial setup

--- frontend/pages/dashboard/index.tsx
+++ frontend/pages/dashboard/index.tsx
@@ -1,4 +1,19 @@
 import { Dashboard } from "@/components/Dashboard"
+import { initializeApollo, addApolloState } from "@/utils/apolloClient"
+import { CURRENT_USER_QUERY } from "@/graphql"
+
 export default function DashboardPage() {
   return <Dashboard />
+}
+
+export async function getServerSideProps() {
+  const apolloClient = initializeApollo()
+  
+  await apolloClient.query({
+    query: CURRENT_USER_QUERY
+  })
+  
+  addApolloState(apolloClient, {
+    props: {}
+  })
 }
