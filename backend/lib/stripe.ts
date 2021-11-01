7472160ae984750357124281408e87821898b47c
Benjamin Paige <benjamin.paige@gmail.com>
2021-10-31 18:10:00 -0600

    Remove backend

--- backend/lib/stripe.ts
+++
@@ -1,5 +0,0 @@
-import Stripe from 'stripe'
-
-export const stripeConfig = new Stripe(process.env.STRIPE_SECRET || '', {
-    apiVersion: "2020-08-27"
-})
--- backend/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './Message';
-export * from './MessageImage';
-export * from './User';
-export * from './Letter';
-export * from './CartItem';
-export * from './OrderItem';
-export * from './Order';
