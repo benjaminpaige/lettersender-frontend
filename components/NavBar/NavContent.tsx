41f8e8963b28bd221be646af381aca47347379cf
Benjamin Paige <benjamin.paige@gmail.com>
2021-11-08 19:49:46 -0700

    Add color mode button in navbar

--- components/NavBar/NavContent.tsx
+++ components/NavBar/NavContent.tsx
@@ -6,8 +6,11 @@ import {
   HStack,
   useDisclosure,
   VisuallyHidden,
+  IconButton,
+  useColorMode,
   useColorModeValue as mode
 } from "@chakra-ui/react"
+import { FaMoon, FaSun } from "react-icons/fa"
 import { Logo } from "../Logo"
 import { NavLink } from "./NavLink"
 import { NavMenu } from "./NavMenu"
@@ -27,6 +30,7 @@ const MobileNavContext = ({
   ...flexProps
 }: NavContentProps) => {
   const { isOpen, onToggle } = useDisclosure()
+  const { toggleColorMode } = useColorMode()
   return (
     <>
       <Flex
@@ -48,6 +52,12 @@ const MobileNavContext = ({
         >
           <Logo />
         </Box>
+        <IconButton
+          aria-label="Toggle Dark Mode"
+          variant="ghost"
+          icon={mode(<FaMoon />, <FaSun />)}
+          onClick={toggleColorMode}
+        />
       </Flex>
       <NavMenu animate={isOpen ? "open" : "closed"}>
         {çNavBar.links.map((link, idx) =>
